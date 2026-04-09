13a3fd0e78b5c200fd2e8915a8d48fd5d58fb806
Vladim72m <vladim72dx@tutamail.com>
2026-04-09 16:52:18 +0300

    Update graf_stat.py

--- graf_stat.py
+++ graf_stat.py
@@ -1,24 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 class GrafStat:
     def __init__(self, csv_path: str):
         self.df = pd.read_csv(csv_path)
 
     def plot_pay_system_pie(self):
-        # Группируем по типу карты
-        grouped = self.df.groupby("Account Name")["Amount"].sum()
 
-        # Цвета как на скрине
-        colors = [
+        grouped = self.df.groupby("Account Name")["Amount"].sum()     # Группируем данные по названию платёжной системы и суммируем траты
+    
+        colors = [        # Набор цветов для сегментов диаграммы
             "#2E7D32",  # тёмно-зелёный
             "#4CAF50",  # зелёный
             "#64B5F6",  # голубой
             "#FFEB3B"   # жёлтый
         ]
 
-        # Создаём donut chart
         fig, ax = plt.subplots(figsize=(8, 8))
 
         wedges, texts, autotexts = ax.pie(
@@ -27,16 +24,15 @@ class GrafStat:
             autopct="%1.0f%%",
             colors=colors[:len(grouped)],
             startangle=90,
-            pctdistance=0.8,     # проценты ближе к центру
+            pctdistance=0.8,
             textprops={"fontsize": 14, "weight": "bold"}
         )
 
-        # Делаем дырку в центре (donut)
-        centre_circle = plt.Circle((0, 0), 0.55, fc="white")
+        centre_circle = plt.Circle((0, 0), 0.55, fc="white")   # Добавляем белый круг в центре
         fig.gca().add_artist(centre_circle)
 
-        # Заголовок
-        plt.title(
+        plt.title(        # Заголовок диаграммы
+   
             "Траты по типу платежной системы ( в процентах % )",
             fontsize=18,
             weight="bold",
@@ -45,3 +41,4 @@ class GrafStat:
 
         plt.tight_layout()
         plt.show()
+        
